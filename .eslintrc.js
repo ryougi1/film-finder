846a58e3e2daf58788a028193af99feeaf1c66f5
ryougi1 <gulong.james@gmail.com>
2020-08-29 14:03:27 +0200

    ISSUE#4: configured styled components properly

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-props-no-spreading': [0],
   },
   globals: {
     fetch: false,
