2d0e547dec9d734fb3180cb6778331b39f9dd768
ryougi1 <gulong.james@gmail.com>
2020-09-01 23:11:15 +0200

    CONF: PWA start

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   pwa: {
     dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    scope: '/app',
+    sw: 'service-worker.js',
   },
 });
